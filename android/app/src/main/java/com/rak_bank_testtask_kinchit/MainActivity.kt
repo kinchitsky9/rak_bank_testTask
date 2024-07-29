541af4e95fce48502efbfa25ec4a2b04571e8a95
kinchitsky9 <kinchit@sky9itcraft>
2024-07-29 12:21:13 +0530

    first commit

---
+++ android/app/src/main/java/com/rak_bank_testtask_kinchit/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.rak_bank_testtask_kinchit
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "rak_bank_testTask_kinchit"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
